11540186f1991d109ea2433a70a3be7764b60310
Antonio Lucas <lucasr.matsunaga@gmail.com>
2025-05-26 19:03:58 -0300

    Feat: ADD Modal

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
 import { db } from '@/src/firebase';
+import { Feather } from '@expo/vector-icons';
 import { useFocusEffect, useRouter } from 'expo-router';
 import { collection, getDocs } from 'firebase/firestore';
 import React, { useCallback, useState } from "react";
-import { FlatList, StyleSheet, Text, TouchableOpacity, useColorScheme, View } from "react-native";
+import { FlatList, Modal, Pressable, StyleSheet, Text, TouchableOpacity, useColorScheme, View } from "react-native";
+
 
 
 export default function ListaConatos () {
@@ -14,6 +16,10 @@ export default function ListaConatos () {
     };
 
     const [contatos, setContatos] = useState<Contato[]>([]);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectContact, setSelectContact] = useState<Contato | null>(null);
+
+
     const router = useRouter();
     const colorSheme = useColorScheme();
     const textColor = colorSheme === 'dark' ? 'white' : 'black';
@@ -38,30 +44,78 @@ export default function ListaConatos () {
     }, [])
   );
 
+  const openModal = (contato: Contato) => {
+    setSelectContact(contato);
+    setModalVisible(true);
+  };
 
-    return (
-        <View style={[styles.container, {backgroundColor: black}]}>
-            <Text style={[styles.titulo, {color: textColor}]}>Agenda Compartilhada</Text>
-            <FlatList
-                data={contatos}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                    <TouchableOpacity style={[styles.item, {backgroundColor: itemBackground}]}>
-                        <Text style={[styles.nome, {color: textColor}]}>{item.nome}</Text>
-                        <Text style={{color: textColor}}>{item.telefone}</Text>
-                    </TouchableOpacity>
-                )}
-            />    
+  const  closeModal = () => {
+    setModalVisible(false);
+    setSelectContact(null);
+  }
 
-            <TouchableOpacity 
-                style={styles.customButton}
-                onPress={() => router.push('/cadastro')}
-            >
-                <Text  style={styles.buttonText}>+</Text>
-            </TouchableOpacity>
+
+  return (
+    <View style={[styles.container, {backgroundColor: black}]}>
+      <Text style={[styles.titulo, {color: textColor}]}>Agenda Compartilhada</Text>
+      
+      <FlatList
+        data={contatos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+        <View style={[
+          styles.item,
+          {
+            backgroundColor: itemBackground,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }
+        ]}>    
+          <View>      
+              <Text style={[styles.nome, {color: textColor}]}>{item.nome}</Text>
+              <Text style={{color: textColor}}>{item.telefone}</Text>
+            </View>
+              <TouchableOpacity onPress={() => openModal(item)}>
+                <Feather name='eye' size={24} color={textColor} />
+              </TouchableOpacity>
+          </View>
+        )}
+      />    
+
+      <TouchableOpacity 
+        style={styles.customButton}
+        onPress={() => router.push('/cadastro')}
+      >
+        <Text  style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+   
+
+      <Modal
+          visible={modalVisible}
+          transparent={true}
+          animationType='slide'
+      >
+        <View style={styles.modalOverlay}>
+          <View style={[styles.modalContent, {backgroundColor: itemBackground}]}>
+            <Text style={[styles.modalTitle, {color: textColor}]}>Detalhes do Contato</Text>
+            {selectContact && (
+              <>
+                <Text style={{color: textColor}}>Nome: {selectContact.nome}</Text>
+                <Text style={{color: textColor}}>Telefone: {selectContact.telefone}</Text>
+                <Text style={{color: textColor}}>E-mail: {selectContact.email}</Text>
+              </>
+            )}
+            
+            <Pressable style={styles.closeButton} onPress={closeModal}>
+              <Text style={{ color: 'white' }}>Fechar</Text>
+            </Pressable>
+
+          </View>
         </View>
-    )
-
+      </Modal>
+  </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -105,4 +159,29 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalContent: {
+    width: '80%',
+    padding: 20,
+    borderRadius: 10,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+
+  },
+  closeButton: {
+    marginTop: 20,
+    backgroundColor: 'orange',
+    padding: 10,
+    borderRadius: 8, 
+    alignItems: 'center'
+  },
+
 });
